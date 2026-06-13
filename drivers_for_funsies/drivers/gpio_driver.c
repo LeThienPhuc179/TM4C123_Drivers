1b7a4610a49a7eb637f4102418b2da0d1bdd90a8
LeThienPhuc179 <128790329+LeThienPhuc179@users.noreply.github.com>
2026-06-12 21:33:11 -0400

    output completed

--- drivers_for_funsies/drivers/gpio_driver.c
+++ drivers_for_funsies/drivers/gpio_driver.c
@@ -9,87 +9,92 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
+
+static inline GPIO_TypeDef* 
+GET_GPIO_BASE(PORT_TYPE type, GPIO_PORT port){
+    GPIO_TypeDef *base_addr = NULL;
+    if (type == AHB){
+        base_addr = AHB_TABLE[port];
+    } else {
+        base_addr = APB_TABLE[port];
+    }
+    return base_addr;
+};
 
 void
 GPIO_init(GPIO_PORT port, PORT_TYPE type, uint8_t pin, PIN_DIR direction){
     (*((volatile uint32_t *)(SYS_CONTROL_BASE_ADDR + RCGCGPIO))) |= (1U << port);
+   
     uint32_t PR_GPIO = NULL;
-
     do {
         PR_GPIO = (*((volatile uint32_t *)(SYS_CONTROL_BASE_ADDR + PRGPIO))) & (1U << port);
     } while (PR_GPIO == 0);
 
-    GPIO_TypeDef *gpio_port = NULL;
-
-    switch (type){
-        case APB:
-            switch (port){
-                case PORT_A:
-                    gpio_port = GPIO_PORTAP_BASE_ADDR;
-                    break;
-                case PORT_B:
-                    gpio_port = GPIO_PORTBP_BASE_ADDR;
-                    break;
-                case PORT_C:
-                    gpio_port = GPIO_PORTCP_BASE_ADDR;
-                    break;
-                case PORT_D:
-                    gpio_port = GPIO_PORTDP_BASE_ADDR;
-                    break;
-                case PORT_E:
-                    gpio_port = GPIO_PORTEP_BASE_ADDR;
-                    break;
-                case PORT_F:
-                    gpio_port = GPIO_PORTFP_BASE_ADDR;
-                    break;
-            }
-            break;
-        case AHB:
-            switch (port){
-                case PORT_A:
-                    gpio_port = GPIO_PORTAH_BASE_ADDR;
-                    break;
-                case PORT_B:
-                    gpio_port = GPIO_PORTBH_BASE_ADDR;
-                    break;
-                case PORT_C:
-                    gpio_port = GPIO_PORTCH_BASE_ADDR;
-                    break;
-                case PORT_D:
-                    gpio_port = GPIO_PORTDH_BASE_ADDR;
-                    break;
-                case PORT_E:
-                    gpio_port = GPIO_PORTEH_BASE_ADDR;
-                    break;
-                case PORT_F:
-                    gpio_port = GPIO_PORTFH_BASE_ADDR;
-                    break;
-            }
-        break;
-    }
-
+    GPIO_TypeDef *gpio_port = GET_GPIO_BASE(type, port);
     gpio_port->DEN |= (1U << pin);
 
     if (direction != OUTPUT){
         if (direction == INPUT){
+            //Configure the pin as Input
             gpio_port->DIR &= ~(1U << pin); 
         } else {
             gpio_port->DIR &= ~(1U << pin);
             if (direction == INPUT_PULLUP){
+                //Configure PULL_UP
                 gpio_port -> PUR |= (1U << pin);
             } else if (direction == INPUT_PULLDOWN){
+                //COnfigure PULL_DOWN
                 gpio_port -> PDR |= (1U << pin);
             }
         }
     } else {
+        // Configure the pin as Output
         gpio_port->DIR |= (1U << pin);
     }
-
     return;
 }
 
-void output_pin(GPIO_PORT port, uint8_t pin, uint8_t val){
-    
+
+void 
+output_pin(GPIO_PORT port, uint8_t pin, uint8_t val){
+    assert(port <= PORT_F   && "GPIO_PORT port input is out of range");
+    assert(pin <= 7         && "pin value is out of range");
+
+    volatile uint32_t port_PR = (*((volatile uint32_t *)(SYS_CONTROL_BASE_ADDR + PRGPIO))) & (1U << port);
+    assert(port_PR != 0     && "Check PORT Initialization");
+
+    GPIO_TypeDef *gpio_port = GET_GPIO_BASE(AHB, port);
+    if (val == 0){
+        gpio_port->DR[1U << pin] = 0x00;
+    } else {
+        gpio_port->DR[1U << pin] = 0xFF;
+    }    
+}
+
+void 
+output_port(GPIO_PORT port, uint8_t val){
+    assert(port <= PORT_F       && "GPIO_PORT port input is out of range");
+    volatile uint32_t port_PR = (*((volatile uint32_t *)(SYS_CONTROL_BASE_ADDR + PRGPIO))) & (1U << port);
+    assert(port_PR != 0         && "Check PORT Initialization");
+
+    GPIO_TypeDef *gpio_port = GET_GPIO_BASE(AHB, port);
+    gpio_port->DR[255] = 0x00;
+    gpio_port->DR[255] |= val;
+}
+
+void
+output_field(GPIO_PORT port, uint8_t mask, uint8_t val){
+    assert(port <= PORT_F       && "GPIO_PORT port input is out of range");
+    volatile uint32_t port_PR = (*((volatile uint32_t *)(SYS_CONTROL_BASE_ADDR + PRGPIO))) & (1U << port);
+    assert(port_PR != 0         && "Check PORT Initialization");
+
+    GPIO_TypeDef *gpio_port = GET_GPIO_BASE(AHB, port);
+    if (val == 0){
+        gpio_port->DR[mask] = 0x00;
+    } else {
+        gpio_port->DR[mask] = 0xFF;
+    }    
 }
 
 
--- drivers_for_funsies/drivers/gpio_driver.h
+++ drivers_for_funsies/drivers/gpio_driver.h
@@ -29,7 +29,26 @@
 #define GPIO_PORTFP_BASE_ADDR   0x40025000
 #define GPIO_PORTFH_BASE_ADDR   0x4005D000
 
+static GPIO_TypeDef* const APB_TABLE[] = {
+    [PORT_A] = GPIO_PORTAP_BASE_ADDR,
+    [PORT_B] = GPIO_PORTBP_BASE_ADDR,
+    [PORT_C] = GPIO_PORTCP_BASE_ADDR,
+    [PORT_D] = GPIO_PORTDP_BASE_ADDR,
+    [PORT_E] = GPIO_PORTEP_BASE_ADDR,
+    [PORT_F] = GPIO_PORTFP_BASE_ADDR,
+};
 
+
+static GPIO_TypeDef* const AHB_TABLE[] = {
+    [PORT_A] = GPIO_PORTAH_BASE_ADDR,
+    [PORT_B] = GPIO_PORTBH_BASE_ADDR,
+    [PORT_C] = GPIO_PORTCH_BASE_ADDR,
+    [PORT_D] = GPIO_PORTDH_BASE_ADDR,
+    [PORT_E] = GPIO_PORTEH_BASE_ADDR,
+    [PORT_F] = GPIO_PORTFH_BASE_ADDR,
+};
+
+static inline GPIO_TypeDef* GET_GPIO_BASE(PORT_TYPE type, GPIO_PORT port);
 
 typedef enum {
     APB,
@@ -53,39 +72,38 @@ typedef enum {
 }PIN_DIR;
 
 typedef struct {
-    volatile uint32_t DR[256];  //0x000 to 0x3FC
-    volatile uint32_t DIR;      //0x400
-    volatile uint32_t IS;       //0x404
-    volatile uint32_t IBE;      //0x408
-    volatile uint32_t IEV;      //0x40C
-    volatile uint32_t IM;       //0x410
-    volatile uint32_t RIS;      //0x414
-    volatile uint32_t MIS;      //0x418
-    volatile uint32_t ICR;      //0x41C
-    volatile uint32_t AFSEL;    //0x420
+    volatile uint32_t DR[256];      //0x000 to 0x3FC
+    volatile uint32_t DIR;          //0x400
+    volatile uint32_t IS;           //0x404
+    volatile uint32_t IBE;          //0x408
+    volatile uint32_t IEV;          //0x40C
+    volatile uint32_t IM;           //0x410
+    volatile uint32_t RIS;          //0x414
+    volatile uint32_t MIS;          //0x418
+    volatile uint32_t ICR;          //0x41C
+    volatile uint32_t AFSEL;        //0x420
     volatile uint32_t _reserved[55];//padding to 0x500
-    volatile uint32_t DR2R;     //0x500
-    volatile uint32_t DR4R;     //0x504
-    volatile uint32_t DR8R;     //0x508
-    volatile uint32_t ODR;      //0x50C
-    volatile uint32_t PUR;      //0x510
-    volatile uint32_t PDR;      //0x514
-    volatile uint32_t SLR;      //0x518
-    volatile uint32_t DEN;      //0x51C
-    volatile uint32_t LOCK;     //0x520
-    volatile uint32_t CR;       //0x524
-    volatile uint32_t AMSEL;    //0x528
-    volatile uint32_t PCTL;     //0x52C
-    volatile uint32_t ADCCTL;   //0x530
-    volatile uint32_t DMACTL;   //0x534
+    volatile uint32_t DR2R;         //0x500
+    volatile uint32_t DR4R;         //0x504
+    volatile uint32_t DR8R;         //0x508
+    volatile uint32_t ODR;          //0x50C
+    volatile uint32_t PUR;          //0x510
+    volatile uint32_t PDR;          //0x514
+    volatile uint32_t SLR;          //0x518
+    volatile uint32_t DEN;          //0x51C
+    volatile uint32_t LOCK;         //0x520
+    volatile uint32_t CR;           //0x524
+    volatile uint32_t AMSEL;        //0x528
+    volatile uint32_t PCTL;         //0x52C
+    volatile uint32_t ADCCTL;       //0x530
+    volatile uint32_t DMACTL;       //0x534
 } GPIO_TypeDef;
-
-
 
 void GPIO_Init(GPIO_PORT port, PORT_TYPE type, uint8_t pin, PIN_DIR direction);
 
 // DEFAULT AHB PORTS
 uint8_t read_pin    (GPIO_PORT port, uint8_t pin);
+
 uint8_t read_field  (GPIO_PORT port, uint8_t mask);
 uint8_t read_port   (GPIO_PORT port);
 
@@ -93,9 +111,16 @@ uint8_t read_pin_APB    (GPIO_PORT port, uint8_t pin);
 uint8_t read_field_APB  (GPIO_PORT port, uint8_t mask);
 uint8_t read_port_APB   (GPIO_PORT port);  
 
-// DEFAULT AHB PORTS
+//DEFAULT AHB PORTS
+//  output_pin: output 0 if val is 0, 1 if otherwise
 void output_pin     (GPIO_PORT port, uint8_t pin, uint8_t val);
+
+//output_field: mask to pick which pins being affected, 
+//                 output 0 if val is 0, 1 if otherwise
 void output_field   (GPIO_PORT port, uint8_t mask, uint8_t val);
+
+//output_port: for funsies, no one will ever use this
+//            This is prolly the most useless function of them all
 void output_port    (GPIO_PORT port, uint8_t val);
 
 void output_pin_APB     (GPIO_PORT port, uint8_t pin, uint8_t val);
@@ -104,4 +129,4 @@ void output_port_APB    (GPIO_PORT port, uint8_t val);
 
 void GPIO_Interrupt_Init();
 
-#endif 
+#endif
